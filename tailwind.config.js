580e6576ddaa4895597933d2aa5da27b846e29b9
Yavor <fomalsstar@gmail.com>
2025-09-25 00:49:21 +0700

    sdf232122

--- tailwind.config.js
+++ tailwind.config.js
@@ -2,7 +2,12 @@
 export default {
   content: ['./index.html', './src/**/*.{js,ts,jsx,tsx}'],
   theme: {
-    extend: {},
+    extend: {
+      fontFamily: {
+        sans: ['Inter', 'ui-sans-serif', 'system-ui', 'Segoe UI', 'Roboto', 'Helvetica Neue', 'Arial', 'Noto Sans', 'Apple Color Emoji', 'Segoe UI Emoji', 'Segoe UI Symbol'],
+        display: ['Poppins', 'Inter', 'ui-sans-serif', 'system-ui']
+      }
+    },
   },
   plugins: [],
 };
